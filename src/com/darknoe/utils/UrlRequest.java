78bec63bd24df1dd9ce9ef4a6aa097dd94308a67
Darknoe <noe6600@gmail.com>
2014-05-02 18:07:00 +0200

    New method "isOnline"

--- src/com/darknoe/utils/UrlRequest.java
+++ src/com/darknoe/utils/UrlRequest.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -18,18 +18,40 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
 
 public class UrlRequest {
 
-	public static String GET(String url) throws URISyntaxException, ClientProtocolException, IOException{
+	/** 
+	 * Returns string GET response. Headers can be null.
+	 * @author Darknoe
+	 * @return responseContent
+	 */
+	public static String GET(String url, HashMap<String, String> headers) throws URISyntaxException, ClientProtocolException, IOException{
 		String responseContent = null;
 		
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet();
 		request.setURI(new URI(url)); //Throws URISyntaxException
+		
+		//Set headers if we have
+		if(headers!=null){
+			Iterator it = headers.entrySet().iterator();
+		    while (it.hasNext()) {
+		    	HashMap.Entry pairs = (HashMap.Entry)it.next();
+		        it.remove(); // avoids a ConcurrentModificationException
+		        request.addHeader(pairs.getKey().toString(), pairs.getValue().toString());
+		    }
+		}
+		
+		Log.e("DNL: UrlRequest GET", "request");
 		HttpResponse response = client.execute(request); //Throws ClientProtocolException
 		InputStream ips = response.getEntity().getContent();
+		Log.e("DNL: UrlRequest GET", "response");
 		BufferedReader buf = new BufferedReader(new InputStreamReader(ips, "UTF-8"));
 		
 		StringBuilder sb = new StringBuilder();
@@ -50,20 +72,11 @@ public class UrlRequest {
 	
 	public static String POST(String url, List<NameValuePair> properties) throws ClientProtocolException, IOException {
 		String responseContent = null;
-	    // Create a new HttpClient and Post Header
 	    HttpClient httpclient = new DefaultHttpClient();
 	    HttpPost httppost = new HttpPost(url);
 
-        // Add your data
-	    /*
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-        nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
-        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs)); //Throws UnsupportedEncodingException
-        */
 	    httppost.setEntity(new UrlEncodedFormEntity(properties)); //Throws UnsupportedEncodingException
 
-        // Execute HTTP Post Request
         HttpResponse response = httpclient.execute(httppost); //Throws ClientProtocolException
         InputStream ips = response.getEntity().getContent();
 		BufferedReader buf = new BufferedReader(new InputStreamReader(ips, "UTF-8"));
@@ -82,5 +95,29 @@ public class UrlRequest {
 		responseContent = sb.toString();
 	    
 	    return responseContent;
-	} 
+	}
+	
+	/** 
+	 * Function that returns TRUE if the device is online
+	 * @author Darknoe
+	 * @param context
+	 * @return boolean
+	 */
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager)
+        context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            Log.e("Conection checker", "connectivity = null");
+        } else {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
